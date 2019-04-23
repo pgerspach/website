fc570b2362ff0704fc2c2d8446bf7fcb952cbe23
pgerspach <pgerspac@alumni.nd.edu>
2019-04-23 16:23:22 -0400

    Updates

---
+++ precache-manifest.b33a00649cdf0f31651311d11f82eaa5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "04ef96c43a8e6ae686d2",
+    "url": "/website/static/css/main.8639e1f4.chunk.css"
+  },
+  {
+    "revision": "04ef96c43a8e6ae686d2",
+    "url": "/website/static/js/main.3f111c73.chunk.js"
+  },
+  {
+    "revision": "738c5e760cdb5d1c1801",
+    "url": "/website/static/js/runtime~main.0439b053.js"
+  },
+  {
+    "revision": "15687dc93a9974c4275c",
+    "url": "/website/static/js/2.15730a4e.chunk.js"
+  },
+  {
+    "revision": "019877c6bda27fd2349b2b4d184f827c",
+    "url": "/website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.a75a6202f87fc7df727ffdafbaa0abd7.js"
+  "/website/precache-manifest.b33a00649cdf0f31651311d11f82eaa5.js"
 );
 
 workbox.clientsClaim();
